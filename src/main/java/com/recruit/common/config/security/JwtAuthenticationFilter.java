5f99f2f195ceccd9f7d5d46f35d1aee7586b8ea1
cw7430 <cw282@naver.com>
2026-07-05 18:07:26 +0900

    디렉토리 변경 및 import 최적화

--- src/main/java/com/recruit/common/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/recruit/common/config/security/JwtAuthenticationFilter.java
@@ -8,11 +8,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import java.io.IOException;
 import java.util.List;
